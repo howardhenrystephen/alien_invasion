4f753bd9ebeb30cd7955cf6a3326893200f2ab01
Henry Stephen <HenryStephen@163.com>
2024-12-31 15:04:17 +0800

    "Second Commit"

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import sys
 
 import pygame
 
+from setting import Settings
+
 class AlienInvasion:
     """管理游戏资源和行为的类"""
     def __init__(self):
@@ -12,12 +14,13 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
 
         # pygame's surface
-        self.screen = pygame.display.set_mode((900, 600))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_length)
+        )
         # 窗口名
         pygame.display.set_caption("Alien Invasion")
 
-        # 设置背景色
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """开始游戏的主循环"""
@@ -29,7 +32,7 @@ class AlienInvasion:
 
             # 每次循环时都重新绘制屏幕
             # RGB - Red, Green, Blue
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             
             # 让最近绘制的屏幕可见
             pygame.display.flip()
