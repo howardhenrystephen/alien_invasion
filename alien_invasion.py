152e67f9f265de3dfb7f4db1139525b7a0cd5f7e
Henry Stephen <HenryStephen@163.com>
2024-12-31 15:23:24 +0800

    "Forth Commit"

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from setting import Settings
+from ship import Ship
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
@@ -22,6 +23,9 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
 
+        # 初始化飞船类
+        self.ship = Ship(self)
+
     def run_game(self):
         """开始游戏的主循环"""
         while True:
@@ -33,6 +37,7 @@ class AlienInvasion:
             # 每次循环时都重新绘制屏幕
             # RGB - Red, Green, Blue
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             
             # 让最近绘制的屏幕可见
             pygame.display.flip()
