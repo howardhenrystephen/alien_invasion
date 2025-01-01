1b4395282be2f1d62ef8855650e80d4575f74cb2
Henry Stephen <HenryStephen@163.com>
2025-01-01 14:13:04 +0800

    "commit on 14:15, 01/01/2025"

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,15 +10,19 @@ class AlienInvasion:
     def __init__(self):
         """初始化游戏并创建游戏资源"""
         pygame.init()
+        self.settings = Settings()
 
         # 时钟控制帧率
         self.clock = pygame.time.Clock()
 
         # pygame's surface
-        self.settings = Settings()
+
         self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_length)
+            (0, 0),
+            pygame.FULLSCREEN
         )
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         # 窗口名
         pygame.display.set_caption("Alien Invasion")
 
@@ -45,15 +49,27 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                # 响应按下
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                # 响应释放
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """响应按下"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+        
+    def _check_keyup_events(self, event):
+        """响应释放"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
             
     
     def _update_screen(self):
